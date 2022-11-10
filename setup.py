67509e41a2af591b5a25898d18836d10077729f8
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2022-11-10 14:51:46 -0500

    updating version number

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(join(this_dir, "requirements.txt")) as f:
 
 setup(
     name="camps",
-    version="1.0.0",
+    version="2.0.0",
     description="Operational response simulation tool for epidemics within settlements",
     url="https://github.com/UNGlobalPulse/UNGP-settlement-modelling",
     long_description=long_description,
